979275c7bff37202718e6244c818d8ca81cb0acc
Oleg Mordach <omordach@gmail.com>
2018-02-14 10:52:12 +0200

    Fixed typo in sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 "use strict";
 importScripts("sw-toolbox.js");
-toolbox.precache(["index.html","assests/css/main.css","assests/js/main.css","assests/sass/main.scss","images/main.jpg","images/header.jpg"]);
-toolbox.router.get("/images/*", toolbox.cacheFirst); toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
+toolbox.precache(["index.html","assets/css/main.css","assets/js/main.js","assets/sass/main.scss","images/main.jpg","images/header.jpg"]);
+toolbox.router.get("images/*", toolbox.cacheFirst); toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5});
